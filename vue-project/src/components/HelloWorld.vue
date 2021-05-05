3d1ad012875620819e284d9c87b9ce622cad991e
Валерий <rasvv@mail.ru>
2021-05-05 11:18:18 +0300

    Домашнее задание к уроку №1

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hello">
+    <input v-model="a" type="text" />
+    <input v-model="b" type="text" />
+    = {{ result }}
+    <div class="buttons">
+      <button @click="calculate('+')">Сложить</button>
+      <button @click="calculate('-')">Вычесть</button>
+      <button @click="calculate('/')">Разделить</button>
+      <button @click="calculate('*')">Умножить</button>
+      <button @click="calculate('^')">Возвести в степень</button>
+      <button @click="calculate('%')">Целочисленное деление</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    a: 0,
+    b: 0,
+    result: 0
+  }),
+  props: {
+    msg: String,
+    myBool: Boolean
+  },
+  methods: {
+    calculate (fun) {
+      switch (fun) {
+        case '+':
+          this.result = +this.a + +this.b
+          break
+        case '-':
+          this.result = +this.a - +this.b
+          break
+        case '*':
+          this.result = +this.a * +this.b
+          break
+        case '/':
+          this.result = +this.a / +this.b
+          break
+        case '^':
+          this.result = Math.pow(+this.a, +this.b)
+          break
+        case '%':
+          this.result = Math.round(+this.a / +this.b)
+          break
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin-right: 10px;
+}
+
+.buttons {
+  margin-top: 15px;
+}
+</style>
