46fdbf41735243e38a83715b267a105a68bf016f
Валерий <rasvv@mail.ru>
2021-05-11 21:43:37 +0300

    Сделал ДЗ к уроку 2

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="hello">
-    <input v-model="a" type="text" />
-    <input v-model="b" type="text" />
+    <input v-model.lazy="oper1" />
+    <input v-model.number.lazy="oper2" />
     = {{ result }}
+    <br>
+    <br>
+    <input type="checkbox" id="checkbox" v-model="checked" >
+    <label for="checkbox">Вывести экранную клавиатуру </label>
+
+    <div class="numberbuttons" v-show="checked">
+      <input type="radio" id="left" value="Операнд1" v-model="picked" >
+      <label for="left">Операнд1</label>
+
+      <input type="radio" id="right" value="Операнд2" v-model="picked">
+      <label for="right">Операнд2</label>
+      <br>
+
+      <br>
+      <button v-for="num in numbers"  @click="fill(num)" :key="num">
+        {{ num }}
+      </button>
+    </div>
     <div class="buttons">
-      <button @click="calculate('+')">Сложить</button>
-      <button @click="calculate('-')">Вычесть</button>
-      <button @click="calculate('/')">Разделить</button>
-      <button @click="calculate('*')">Умножить</button>
-      <button @click="calculate('^')">Возвести в степень</button>
-      <button @click="calculate('%')">Целочисленное деление</button>
+      <button v-for="opera in operators" @click="calculate(opera)" :key="opera">
+        {{ opera }}
+      </button>
     </div>
   </div>
 </template>
@@ -17,9 +32,14 @@
 <script>
 export default {
   data: () => ({
-    a: 0,
-    b: 0,
-    result: 0
+    oper1: 0,
+    oper2: 0,
+    strOper: '0',
+    result: 0,
+    operators: ['+', '-', '/', '*', '^', '%'],
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '<-'],
+    checked: false,
+    picked: 'Операнд1'
   }),
   props: {
     msg: String,
@@ -27,26 +47,34 @@ export default {
   },
   methods: {
     calculate (fun) {
-      switch (fun) {
-        case '+':
-          this.result = +this.a + +this.b
-          break
-        case '-':
-          this.result = +this.a - +this.b
-          break
-        case '*':
-          this.result = +this.a * +this.b
-          break
-        case '/':
-          this.result = +this.a / +this.b
-          break
-        case '^':
-          this.result = Math.pow(+this.a, +this.b)
-          break
-        case '%':
-          this.result = Math.round(+this.a / +this.b)
-          break
+      const { oper1, oper2 } = this
+      const calcRes = {
+        '+': () => oper1 + oper2,
+        '-': () => oper1 - oper2,
+        '/': () => oper1 / oper2,
+        '*': () => oper1 * oper2,
+        '^': () => Math.pow(oper1, oper2),
+        '%': () => Math.round(oper1 / oper2)
       }
+      this.result = calcRes[fun]()
+    },
+    fill (num) {
+      if (this.picked === 'Операнд1') { this.strOper = this.oper1.toString() } else { this.strOper = this.oper2.toString() }
+
+      if (num === '<-') {
+        if (this.strOper.length === 1) {
+          this.strOper = 0
+        } else {
+          this.strOper = this.strOper.slice(0, -1)
+        }
+      } else {
+        if (this.strOper === '0') {
+          this.strOper = num.toString()
+        } else {
+          this.strOper = this.strOper.concat(num.toString())
+        }
+      }
+      if (this.picked === 'Операнд1') { this.oper1 = +this.strOper } else { this.oper2 = +this.strOper }
     }
   }
 }
