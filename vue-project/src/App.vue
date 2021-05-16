55e142fdd9ad109ab4e2272306c2e723a4ac67f6
Валерий <rasvv@mail.ru>
2021-05-16 16:55:35 +0300

    Домашнее задание к уроку №3

---
+++ vue-project/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <header :class="[$style.header]">
+      Мой подсчет расходов
+    </header>
+    <main>
+      <button @click="onChangeVisibleForm">Добавить данные</button>
+      <PaymentsForm @add="onDatAdded" v-show="checked"/>
+      <PaymentsList :items="paymentsList"/>
+    </main>
+  </div>
+</template>
+
+<script>
+import PaymentsList from './components/PaymentsList.vue'
+import PaymentsForm from './components/PaymentsForm.vue'
+export default {
+  name: 'App',
+  components: {
+    PaymentsList,
+    PaymentsForm
+  },
+  data () {
+    return {
+      checked: false,
+      paymentsList: [
+        { date: '2021-05-01', category: 'еда', price: 754 },
+        { date: '2021-05-02', category: 'одежда', price: 2500 },
+        { date: '2021-05-03', category: 'заправка', price: 2000 },
+        { date: '2021-05-04', category: 'еда', price: 754 },
+        { date: '2021-05-05', category: 'одежда', price: 2500 },
+        { date: '2021-05-06', category: 'заправка', price: 2000 },
+        { date: '2021-05-07', category: 'еда', price: 754 },
+        { date: '2021-05-08', category: 'одежда', price: 2500 },
+        { date: '2021-05-11', category: 'заправка', price: 2000 },
+        { date: '2021-05-12', category: 'проезд', price: 49 }
+      ]
+    }
+  },
+  methods: {
+    onDatAdded (data) {
+      this.paymentsList.push(data)
+    },
+    onChangeVisibleForm () {
+      this.checked = !this.checked
+    }
+  }
+}
+</script>
+
+<style lang="sass" module>
+.header
+  color: red
+  font-size: 24px
+
+button
+  margin: 10px 0
+</style>
---
+++ vue-project/src/components/PaymentsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list">
+    <div class="list__table" :style="style">
+      <div class="list__header">
+        <div class="list__header-cell date">Дата</div>
+        <div class="list__header-cell category">Категория</div>
+        <div class="list__header-cell price">Цена</div>
+      </div>
+
+      <div class="list__data" v-for="(item, index) in paginatedData" :key="index" >
+        <div class="list__data-cell date">{{ item.date }}</div>
+        <div class="list__data-cell category">{{ item.category }}</div>
+        <div class="list__data-cell price">{{ item.price }}</div>
+      </div>
+    </div>
+    <div class="btn-toolbar">
+      <button
+      v-for="page in pageCount"
+      v-bind:key="page"
+      v-bind:class="{ active: page === currentlyPage }"
+      @click.prevent="setPage(page)">
+        {{ page }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script src="./PaymentsList.js"></script>
+
+<style src="./PaymentsList.sass"  lang='sass'></style>
