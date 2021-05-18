4d9d2e6a91b704786bc2e757fbade6246fe19c28
rasvv <rasvv@mail.ru>
2021-05-18 17:06:43 +0300

    начал изучать vues

---
+++ vue-project/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    paymentsList: []
+  },
+	mutations: {
+		setPaymentsListData (state, payload) {
+			state.paymentsList = payload
+		}
+	}
+})
